f435d9aed6f840bd15d353bc98379d1d0398182c
stanislav.grigoriev <grigorievstas9@gmail.com>
2020-05-05 18:50:39 +0700

    Add scaler, reshape layers

---
+++ Source/EasyCNTK/Layers/Reshape.cs
@@ -0,0 +1,43 @@
+﻿//
+// Copyright (c) Stanislav Grigoriev. All rights reserved.
+// grigorievstas9@gmail.com 
+// https://github.com/StanislavGrigoriev/EasyCNTK
+//
+// Copyright (c) Microsoft. All rights reserved.
+//
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+//
+using CNTK;
+using System.Linq;
+
+namespace EasyCNTK.Layers
+{
+    public sealed class Reshape : Layer
+    {
+        int[] _targetShape;
+        private string _name;
+        public Reshape(int[] targetShape, string name = "Reshape")
+        {
+            _targetShape = targetShape;
+            _name = name;
+        }
+        public override Function Create(Function input, DeviceDescriptor device)
+        {
+            var resahaped = CNTKLib.Reshape(input, _targetShape, _name);
+
+            return resahaped;
+        }
+
+        public override string GetDescription()
+        {
+            var shape = "";
+            _targetShape.ToList().ForEach(p =>
+            {
+                shape += p.ToString() + "x";
+            });
+            shape = shape.Substring(0, shape.Length - 1);
+
+            return $"Reshape({shape})";
+        }
+    }
+}
---
+++ Source/EasyCNTK/Layers/Scaler.cs
@@ -0,0 +1,40 @@
+﻿//
+// Copyright (c) Stanislav Grigoriev. All rights reserved.
+// grigorievstas9@gmail.com 
+// https://github.com/StanislavGrigoriev/EasyCNTK
+//
+// Copyright (c) Microsoft. All rights reserved.
+//
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+//
+using CNTK;
+
+namespace EasyCNTK.Layers
+{
+    /// <summary>
+    /// Реализует масштабирование выхода путем перемножения на константу
+    /// </summary>
+    public sealed class Scaler : Layer
+    {
+        double _factor;
+        /// <summary>
+        /// Масштабирует выход перемножая каждый элемент на константу
+        /// </summary>
+        /// <param name="factor">Констатнта, на которую перемножается выход</param>
+        public Scaler(double factor)
+        {
+            _factor = factor;
+        }
+        public override Function Create(Function input, DeviceDescriptor device)
+        {
+            var factor = Constant.Scalar(input.Output.DataType, _factor, device);
+            var scaled = CNTKLib.ElementTimes(factor, input);
+            return scaled;
+        }
+
+        public override string GetDescription()
+        {
+            return $"Scale({_factor})";
+        }
+    }
+}
